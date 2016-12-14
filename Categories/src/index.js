ec18d1b91b22ea8204f843f2ac5a779b158f058b
DmytroMysak <dmytro.mysak@gmail.com>
2016-12-14 13:58:04 +0200

    add products db

--- Categories/src/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-module.exports = {
-    Treebeard: require('./components/treebeard'),
-    decorators: require('./components/decorators'),
-    animations: require('./themes/animations'),
-    theme: require('./themes/default')
-};
